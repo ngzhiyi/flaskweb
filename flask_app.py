9a360a5b52cf6ebb8a29a0bb239580ec16d59dc0
zhiyi <ngzhiyi92@gmail.com>
2023-11-06 05:18:28 +0000

    Added database support

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,23 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="ngzhiyi",
+    password="sctp01!@$",
+    hostname="ngzhiyi.mysql.pythonanywhere-services.com",
+    databasename="ngzhiyi$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
